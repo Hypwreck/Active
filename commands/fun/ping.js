12ed83d2dd2e03e1bc16781815809f2248cbafaf
Hypwreck <89440730+Hypwreck@users.noreply.github.com>
2022-01-18 20:47:44 +0500

    Add files via upload

---
+++ commands/fun/ping.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require('discord.js');
+const config = require('../../config.json');
+module.exports = {
+	name: 'ping',
+	category: 'Misc',
+	description: 'Returns Latency and API Ping',
+	cooldown: 10000,
+	run: async (client, message, args) => {
+		const msg = await message.channel.send({content:'Pinging...'});
+		const Embed = new MessageEmbed()
+			.setTitle('Pong!')
+			.setAuthor(`${message.author.username}`, message.author.displayAvatarURL())
+			.setDescription(
+				`⌛ Latency is ${Math.floor(
+					msg.createdTimestamp - message.createdTimestamp,
+				)}ms\n⏲️ API Ping is ${Math.round(client.ws.ping)}`,
+			)
+			.setColor(config.colour);
+		await msg.edit('\u200b');
+		return msg.edit({embeds: [Embed]});
+	},
+};
