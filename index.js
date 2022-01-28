76e872a755db6de929ae3b1a70c07eff3bfc7f90
Hypwreck <89440730+Hypwreck@users.noreply.github.com>
2022-01-28 11:46:06 +0500

    Update and rename activity.js to index.js

---
+++ index.js
@@ -0,0 +1,106 @@
+const Discord = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
+const { DiscordTogether } = require('discord-together');
+const config = require('./config.json')
+const express = require('express')
+const app = express()
+const db = require('quick.db')
+const colors = require("colors");
+const client = new Discord.Client({
+    fetchAllMembers: true,
+    shards: "auto",
+    allowedMentions: {
+      parse: [ ],
+      repliedUser: false,
+    },
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+    intents: [ 
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
+        Discord.Intents.FLAGS.GUILD_BANS,
+        Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+        Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+        Discord.Intents.FLAGS.GUILD_INVITES,
+        Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+        Discord.Intents.FLAGS.GUILD_PRESENCES,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        Discord.Intents.FLAGS.DIRECT_MESSAGES,
+        Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING
+    ],
+    presence: {
+      activity: {
+        name: `Youtube Together`, 
+        type: "WATCHING", 
+      },
+      status: "online"
+    }
+});
+app.listen(3000)
+    client.guilds.cache.forEach(guild => {
+  console.log(`${guild.name} | ${guild.id}`);
+})
+client.commands = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
+client.slashCommands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.categories = require("fs").readdirSync(`./commands`);
+["events", "commands"]
+    .filter(Boolean)
+    .forEach(h => {
+        require(`./handlers/${h}`)(client);
+    })
+client.discordTogether = new DiscordTogether(client);
+
+
+client.queue = new Map()
+process.on('unhandledRejection', console.error);
+
+
+client.on("messageCreate", async message => {
+ let prefix;
+        try {
+            let fetched = await db.fetch(`prefix_${message.guild.id}`);
+            if (fetched == null) {
+                prefix = config.prefix
+            } else {
+                prefix = fetched
+            }
+        
+            } catch {
+            prefix = config.prefix
+    };
+
+
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  if (!message.member)
+    message.member = message.guild.fetchMember(message);
+
+  const args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+if (!cmd) return
+    if (cmd.inVoiceChannel && !message.member.voice.channel) {
+        return message.channel.send({content: `| You must be in a voice channel!`})
+    }
+  if (cmd.length === 0) return;
+
+  let command = client.commands.get(cmd);
+
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+  if (command) command.run(client, message, args);
+});
+
+
+ 
+
+client.login(config.token || process.env.token);
