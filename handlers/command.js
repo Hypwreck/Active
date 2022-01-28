8216a214fc4365957da7221e5c9868fa478d99ed
Hypwreck <89440730+Hypwreck@users.noreply.github.com>
2022-01-28 11:47:16 +0500

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -1,32 +1,26 @@
-const { readdirSync } = require("fs");
-
-const ascii = require("ascii-table");
-
-
-let table = new ascii("Commands");
-table.setHeading("Command", "Load status");
-
+const {
+    readdirSync
+} = require("fs");
+console.log("Message Commands".yellow);
 module.exports = (client) => {
-  
-    readdirSync("./commands/").forEach(dir => {
-      
-        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
-    
-       
-        for (let file of commands) {
-            let pull = require(`../commands/${dir}/${file}`);
-    
-            if (pull.name) {
-                client.commands.set(pull.name, pull);
-                table.addRow(file, '✅');
-            } else {
-                table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
-                continue;
+    try {
+        let amount = 0;
+        readdirSync("./commands/").forEach((dir) => {
+            const commands = readdirSync(`./commands/${dir}/`).filter((file) => file.endsWith(".js"));
+            for (let file of commands) {
+                let pull = require(`../commands/${dir}/${file}`);
+                if (pull.name) {
+                    client.commands.set(pull.name, pull);
+                    amount++;
+                } else {
+                    console.log(file, `error -> missing a help.name, or help.name is not a string.`.brightRed);
+                    continue;
+                }
+                if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach((alias) => client.aliases.set(alias, pull.name));
             }
-    
-            if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
-        }
-    });
-    
-    console.log(table.toString());
-}
+        });
+        console.log(`${amount} Commands Loaded`.brightGreen);
+    } catch (e) {
+        console.log(String(e.stack).bgRed)
+    }
+};
