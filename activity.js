eb9fe6383e301fb2585f2850033db2301e488627
Hypwreck <89440730+Hypwreck@users.noreply.github.com>
2022-01-28 08:49:31 +0500

    Update activity.js

--- activity.js
+++ activity.js
@@ -34,6 +34,7 @@ process.on('unhandledRejection', console.error);
 ["command", "slashcommand"].forEach(handler => {
   require(`./handlers/${handler}`)(client);
 });
+
 client.on("messageCreate", async message => {
  let prefix;
         try {
@@ -74,7 +75,38 @@ if (!cmd) return
   if (command) command.run(client, message, args);
 });
 
- 
+ client.on("interactionCreate", async (interaction) => {
+    // Slash Command Handling
+    if (interaction.isCommand()) {
+        await interaction.deferReply({ ephemeral : true }).catch(() => {});
+
+        const cmd = client.slashCommands.get(interaction.commandName);
+        if (!cmd)
+            return interaction.followUp({ content: "An error has occured " });
+
+        const args = [];
+
+        for (let option of interaction.options.data) {
+            if (option.type === "SUB_COMMAND") {
+                if (option.name) args.push(option.name);
+                option.options?.forEach((x) => {
+                    if (x.value) args.push(x.value);
+                });
+            } else if (option.value) args.push(option.value);
+        }
+        interaction.member = interaction.guild.members.cache.get(interaction.user.id);
+
+        cmd.run({ client, interaction, args });
+    }
+
+    // Context Menu Handling
+    if (interaction.isContextMenu()) {
+        await interaction.deferReply({ ephemeral: false });
+        const command = client.slashCommands.get(interaction.commandName);
+        if (command) command.run(client, interaction);
+    }
+});
+
 
 console.log('Bot Is Online')
 client.login(config.token || process.env.token);
