df0e7b9d0e53020c1d1b907308ab494bb7b804b3
Hypwreck <89440730+Hypwreck@users.noreply.github.com>
2022-02-16 23:01:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,65 +2,33 @@ const Discord = require('discord.js');
 const { Client, Intents, Collection } = require('discord.js');
 const { DiscordTogether } = require('discord-together');
 const config = require('./config.json')
-const express = require('express')
-const app = express()
 const db = require('quick.db')
-const colors = require("colors");
 const client = new Discord.Client({
     partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-    intents: [ 
-        Discord.Intents.FLAGS.GUILDS,
-        Discord.Intents.FLAGS.GUILD_MEMBERS,
-        Discord.Intents.FLAGS.GUILD_BANS,
-        Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-        Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
-        Discord.Intents.FLAGS.GUILD_WEBHOOKS,
-        Discord.Intents.FLAGS.GUILD_INVITES,
-        Discord.Intents.FLAGS.GUILD_VOICE_STATES,
-        Discord.Intents.FLAGS.GUILD_PRESENCES,
-        Discord.Intents.FLAGS.GUILD_MESSAGES,
-        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
-        Discord.Intents.FLAGS.DIRECT_MESSAGES,
-        Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-        Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING
-    ],
+    intents: [
+                Intents.FLAGS.GUILDS,
+                Intents.FLAGS.GUILD_MESSAGES, 
+                Intents.FLAGS.GUILD_MEMBERS, 
+                Intents.FLAGS.GUILD_VOICE_STATES
+            ],
     presence: {
       status: "online"
     }
 });
-
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Active is now active.')
-})
-
-app.listen(port, () => {
-  console.log(`Listenin to ${port}`)
-})
-
-client.discordTogether = new DiscordTogether(client);
-client.commands = new Discord.Collection();
-client.slash = new Discord.Collection();
-client.aliases = new Discord.Collection();
-require('discord-logs')(client);
-require('colors');
-
-["command", "events", "slash"].forEach(handler => {
-    require(`./handlers/${handler}`)(client);
+client.on('ready', () => {
+    console.log('Bot Online');
 });
 
-process.on('unhandledRejection', (err) => {
-	console.error(`Unhandled Rejection: ${err}`);
-  });
-  
-  process.on('uncaughtException', (err) => {
-	console.error(`Uncaught Exception: ${err}`);
-  });
-  
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 
-// Db for Prefix
+["command"].forEach(handler => {
+  require(`./handlers/${handler}`)(client);
+});
+client.queue = new Map()
+process.on('unhandledRejection', console.error);
+
+  
 client.on("messageCreate", async message => {
  let prefix;
         try {
@@ -88,10 +56,7 @@ client.on("messageCreate", async message => {
     .trim()
     .split(/ +/g);
   const cmd = args.shift().toLowerCase();
-if (!cmd) return
-    if (cmd.inVoiceChannel && !message.member.voice.channel) {
-        return message.channel.send({content: `| You must be in a voice channel!`})
-    }
+
   if (cmd.length === 0) return;
 
   let command = client.commands.get(cmd);
@@ -100,8 +65,5 @@ if (!cmd) return
 
   if (command) command.run(client, message, args);
 });
-
-
- 
 
 client.login(config.token || process.env.token);
